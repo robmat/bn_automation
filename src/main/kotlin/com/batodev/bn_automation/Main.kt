f84c18bb62ccb7c5071d3ed0c8e1fe48e3f66e02
Bator <robbator@gmail.com>
2025-10-31 11:34:46 +0100

    feat: working boar automation

--- src/main/kotlin/com/batodev/bn_automation/Main.kt
+++ src/main/kotlin/com/batodev/bn_automation/Main.kt
@@ -1,11 +1,11 @@
 package com.batodev.bn_automation
 
+import com.batodev.bn_automation.automations.BoarAutomation
 import com.batodev.bn_automation.automations.RaptorAutomation
 
 fun main() {
-    RaptorAutomation().automate()
-    RaptorAutomation().automate()
-    RaptorAutomation().automate()
-    RaptorAutomation().automate()
-    RaptorAutomation().automate()
+    for (i in 1..5) {
+        //BoarAutomation().automate()
+        RaptorAutomation().automate()
+    }
 }
---
+++ src/main/kotlin/com/batodev/bn_automation/automations/BoarAutomation.kt
@@ -0,0 +1,108 @@
+package com.batodev.bn_automation.automations
+
+import com.batodev.bn_automation.imagematch.ImageMatcher
+import com.batodev.bn_automation.imagematch.screenshot
+import com.batodev.bn_automation.logging.logInfo
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import java.awt.Robot
+import java.awt.event.InputEvent
+import javax.imageio.ImageIO
+
+private const val DEFAULT_CLICK_WAIT = 500L
+
+class BoarAutomation {
+    val logger: Logger = LogManager.getLogger(RaptorAutomation::class.java)
+
+    fun automate() {
+        click("/patterns/boar_encounter.png")
+//        Thread.sleep(2000)
+//        dragMouse(1850, 1020, 100, 1020)
+//        Thread.sleep(500)
+//        dragMouse(1850, 1020, 100, 1020)
+        click("/patterns/heavy_tank_pick.png", 1)
+        click("/patterns/heavy_tank_pick.png", 1)
+        click("/patterns/heavy_tank_pick.png", 1)
+        click("/patterns/umg_pick.png", 1)
+        click("/patterns/umg_pick.png", 1)
+        click("/patterns/umg_pick.png", 1)
+        click("/patterns/fight_button.png")
+        while (!ifThereClickIt("/patterns/orange_ok_button.png", 1, 1, 0.05f)) {
+            clickXY(537, 548, 6) // select umg 1
+            clickXY(924, 335, 1) // fire
+            clickXY(701, 623, 6) // select umg 2
+            clickXY(924, 335, 1) // fire
+            clickXY(824, 714, 6) // select umg 3
+            clickXY(924, 335, 1) // fire
+        }
+        click("/patterns/green_ok_button.png")
+    }
+
+    fun drag(fromPatternPath: String, toPatternPath: String, wait: Int = 2, step: Int = 3, maxConfidence: Float = 0.2f) {
+        Thread.sleep(wait * 1000L)
+        val screenshot = screenshot()
+        val fromImage = ImageIO.read(this::class.java.getResourceAsStream(fromPatternPath))
+        val toImage = ImageIO.read(this::class.java.getResourceAsStream(toPatternPath))
+        val fromMatch = ImageMatcher.find(fromImage, screenshot, step, maxConfidence)
+        val toMatch = ImageMatcher.find(toImage, screenshot, step, maxConfidence)
+        logInfo("$fromPatternPath match at (${fromMatch.x}, ${fromMatch.y}) with score ${fromMatch.score}", logger)
+        logInfo("$toPatternPath match at (${toMatch.x}, ${toMatch.y}) with score ${toMatch.score}", logger)
+        dragMouse(fromMatch.x, fromMatch.y, toMatch.x, toMatch.y)
+    }
+
+    private fun dragMouse(fromX: Int, fromY: Int, toX: Int, toY: Int) {
+        val robot = Robot()
+        robot.mouseMove(fromX, fromY)
+        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK)
+        Thread.sleep(DEFAULT_CLICK_WAIT)
+        robot.mouseMove(toX, toY)
+        Thread.sleep(DEFAULT_CLICK_WAIT)
+        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK)
+        logInfo("Dragged from ($fromX, $fromY) to ($toX, $toY)", logger)
+    }
+
+    fun click(patternImagePath: String, wait: Int = 3, step: Int = 3, maxConfidence: Float = 0.2f) {
+        Thread.sleep(wait * 1000L)
+        val screenshot = screenshot()
+        val raptorEncounter = ImageIO.read(this::class.java.getResourceAsStream(patternImagePath))
+        try {
+            val match = ImageMatcher.find(raptorEncounter, screenshot, step, maxConfidence)
+            val x = match.x
+            val y = match.y
+            logInfo("$patternImagePath match at ($x, $y) with score ${match.score}", logger)
+            clickXY(x, y)
+        } catch (e: IllegalStateException) {
+            throw AutomationException("Could not find pattern $patternImagePath on screen with confidence <= $maxConfidence", e)
+        }
+    }
+
+    fun clickXY(x: Int, y: Int, wait: Int = 0) {
+        Thread.sleep(1000L * wait)
+        val robot = Robot()
+        robot.mouseMove(x, y)
+        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK)
+        Thread.sleep(DEFAULT_CLICK_WAIT)
+        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK)
+        logInfo("Clicked at ($x, $y)", logger)
+    }
+
+    fun ifThereClickIt(patternImagePath: String, wait: Int = 3, step: Int = 3, maxConfidence: Float = 0.2f): Boolean {
+        Thread.sleep(wait * 1000L)
+        val screenshot = screenshot()
+        val patternImage = ImageIO.read(this::class.java.getResourceAsStream(patternImagePath))
+        val match = ImageMatcher.find(patternImage, screenshot, step, 1.0f) // Use high maxConfidence to always get a result
+        logInfo("$patternImagePath match at (${match.x}, ${match.y}) with score ${match.score}", logger)
+        if (match.score <= maxConfidence) {
+            val robot = Robot()
+            robot.mouseMove(match.x, match.y)
+            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK)
+            Thread.sleep(DEFAULT_CLICK_WAIT)
+            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK)
+            logInfo("Clicked at (${match.x}, ${match.y})", logger)
+            return true
+        } else {
+            logInfo("No confident match for $patternImagePath (score: ${match.score}, threshold: $maxConfidence)", logger)
+            return false
+        }
+    }
+}
