eba96161dc6d389158a029c263026c220a9789bd
Bator <robbator@gmail.com>
2025-11-01 19:25:18 +0100

    feat: improved animal automation

--- src/main/kotlin/com/batodev/bn_automation/Main.kt
+++ src/main/kotlin/com/batodev/bn_automation/Main.kt
@@ -2,10 +2,38 @@ package com.batodev.bn_automation
 
 import com.batodev.bn_automation.automations.BoarAutomation
 import com.batodev.bn_automation.automations.RaptorAutomation
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import java.awt.Toolkit
+
+val automation = BoarAutomation()
+val logger: Logger = LogManager.getLogger("Main")
 
 fun main() {
-    for (i in 1..5) {
-        //BoarAutomation().automate()
-        RaptorAutomation().automate()
+    if (checkForAnimalEncounters(logger)) {
+        fightAnimalsUntilVisible(logger)
+    } else {
+        // Drag mouse from top to bottom
+        val screenSize = Toolkit.getDefaultToolkit().screenSize
+        automation.dragMouse(screenSize.width / 2, 0, screenSize.width / 2, screenSize.height - 1)
+        if (checkForAnimalEncounters(logger)) {
+            fightAnimalsUntilVisible(logger)
+        } else {
+            // Drag mouse from bottom to top
+            automation.dragMouse(screenSize.width / 2, screenSize.height - 1, screenSize.width / 2, 0)
+            if (checkForAnimalEncounters(logger)) {
+                fightAnimalsUntilVisible(logger)
+            }
+        }
     }
 }
+
+private fun fightAnimalsUntilVisible(logger: Logger) {
+    automation.automate()
+    while (checkForAnimalEncounters(logger)) {
+        automation.automate()
+    }
+}
+
+private fun checkForAnimalEncounters(logger: Logger): Boolean =
+    automation.ifThereClickIt(automation.getEncounterPatternPath(), 1, 1, 0.06f)
