269a108eee418d05dd96dc0f2bf98afd6a3e0266
Bator <robbator@gmail.com>
2025-11-01 20:42:51 +0100

    feat: opencv matcher test

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,11 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.2")
     implementation("org.apache.logging.log4j:log4j-api:2.23.1")
     implementation("org.apache.logging.log4j:log4j-core:2.23.1")
+    implementation("org.openpnp:opencv:4.9.0-0")
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.2")
 }
 
 tasks.test {
--- src/test/kotlin/com/batodev/bn_automation/imagematch/ImageMatcherTest.kt
+++ src/test/kotlin/com/batodev/bn_automation/imagematch/ImageMatcherTest.kt
@@ -1,14 +1,23 @@
 package com.batodev.bn_automation.imagematch
 
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 import org.junit.platform.commons.logging.Logger
 import org.junit.platform.commons.logging.LoggerFactory
+import org.opencv.core.Core
+import org.opencv.core.Core.MinMaxLocResult
+import org.opencv.core.Mat
+import org.opencv.core.Point
+import org.opencv.core.Scalar
+import org.opencv.imgcodecs.Imgcodecs
+import org.opencv.imgproc.Imgproc
 import java.awt.Color
 import java.awt.image.BufferedImage
 import java.io.File
 import java.util.stream.Stream
 import javax.imageio.ImageIO
+
 
 class ImageMatcherTest {
     val logger: Logger = LoggerFactory.getLogger(ImageMatcherTest::class.java)
@@ -27,9 +36,9 @@ class ImageMatcherTest {
     @ParameterizedTest
     @MethodSource("imagePairsProvider")
     fun find(haystackPath: String, needlePath: String, radius: Int) {
-        var imageStream = javaClass.getResourceAsStream("/" + haystackPath)
+        var imageStream = javaClass.getResourceAsStream("/$haystackPath")
         val haystack: BufferedImage = ImageIO.read(imageStream)
-        imageStream = javaClass.getResourceAsStream("/" + needlePath)
+        imageStream = javaClass.getResourceAsStream("/$needlePath")
         val needle: BufferedImage = ImageIO.read(imageStream)
 
         val result = ImageMatcher.find(needle, haystack)
@@ -42,8 +51,68 @@ class ImageMatcherTest {
         g2d.dispose()
 
         // Output the haystack image with the dot to the build folder
-        val outputFile = File("build/${haystackPath}_${needlePath}")
+        val outputFile = File("build/find_${haystackPath}_${needlePath}")
         ImageIO.write(haystack, "png", outputFile)
         logger.info { "Output image written to: ${outputFile.absolutePath}" }
+    }
+
+    @ParameterizedTest
+    @MethodSource("imagePairsProvider")
+    fun openCvMatch(haystackPath: String, needlePath: String, radius: Int) {
+        // Load OpenCV
+        nu.pattern.OpenCV.loadLocally()
+        val haystackMatPath = "src/test/resources/$haystackPath"
+        val needleMatPath = "src/test/resources/$needlePath"
+        val sourceOrig = Imgcodecs.imread(haystackMatPath)
+        val template = Imgcodecs.imread(needleMatPath)
+
+        val matchMethods = listOf(
+            Imgproc.TM_SQDIFF to "TM_SQDIFF",
+            Imgproc.TM_SQDIFF_NORMED to "TM_SQDIFF_NORMED",
+            Imgproc.TM_CCORR to "TM_CCORR",
+            Imgproc.TM_CCORR_NORMED to "TM_CCORR_NORMED",
+            Imgproc.TM_CCOEFF to "TM_CCOEFF",
+            Imgproc.TM_CCOEFF_NORMED to "TM_CCOEFF_NORMED"
+        )
+
+        for ((matchMethod, methodName) in matchMethods) {
+            val source = sourceOrig.clone()
+            val outputImage = Mat()
+            Imgproc.matchTemplate(source, template, outputImage, matchMethod)
+            val mmr: MinMaxLocResult = Core.minMaxLoc(outputImage)
+            val matchLoc: Point = if (matchMethod == Imgproc.TM_SQDIFF || matchMethod == Imgproc.TM_SQDIFF_NORMED) mmr.minLoc else mmr.maxLoc
+
+            // Draw rectangle on result image
+            Imgproc.rectangle(
+                source, matchLoc, Point(
+                    matchLoc.x + template.cols(),
+                    matchLoc.y + template.rows()
+                ), Scalar(0.0, 255.0, 0.0), 3
+            )
+            // Print confidence (score) on the image
+            val confidenceText = String.format("conf: %.3f", if (matchMethod == Imgproc.TM_SQDIFF || matchMethod == Imgproc.TM_SQDIFF_NORMED) mmr.minVal else mmr.maxVal)
+            Imgproc.putText(
+                source,
+                confidenceText,
+                Point(matchLoc.x, matchLoc.y - 10),
+                Imgproc.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                Scalar(0.0, 255.0, 0.0),
+                2
+            )
+            // Print method name under the square
+            Imgproc.putText(
+                source,
+                methodName,
+                Point(matchLoc.x, matchLoc.y + template.rows() + 20),
+                Imgproc.FONT_HERSHEY_SIMPLEX,
+                0.7,
+                Scalar(0.0, 255.0, 0.0),
+                2
+            )
+            // Save result
+            Imgcodecs.imwrite("build/opencv_${methodName}_${haystackPath}_${needlePath}", source)
+            println("OpenCV $methodName match for $haystackPath/$needlePath at (${matchLoc.x}, ${matchLoc.y}) with score $confidenceText")
+        }
     }
 }
